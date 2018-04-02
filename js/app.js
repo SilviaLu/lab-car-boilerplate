8080d6943248e77243a12d1cdbc5ac4f718820a1
SilvaLu <spvv2706@gmail.com>
2018-04-02 03:50:52 -0500

    revisado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 // La funcionalidad de tu proyecto
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: {
+      lat: -12.020651498087096,
+      lng: -76.93456887128904
+    }
+  });
+  directionsDisplay.setMap(map);
+
+  var onChangeHandler = function () {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('search').addEventListener('click', onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: document.getElementById('partida').value,
+    destination: document.getElementById('destino').value,
+    travelMode: 'DRIVING'
+  }, function (response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Debe ingresar de manera correcta sus direcciones');
+    }
+  });
+}
